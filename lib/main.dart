725f02532ca6fab8b3c62807e8bc1bc4079375ff
pRaNaY <www.pranay1337.com@gmail.com>
2023-07-11 19:01:48 +0530

    Complete full screen design

--- lib/main.dart
+++ lib/main.dart
@@ -20,13 +20,68 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
           body: SafeArea(
               child: Center(
-        child: TextButton(
-            onPressed: () {
-              final player = AudioPlayer();
-              player.play(AssetSource('note1.wav'));
-            },
-            child: Text('Play')),
-      ))),
+                  child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [for (int item = 1; item < 8; item++) getItem(item)],
+      )))),
     );
   }
+}
+
+Widget getItem(int index) {
+  return Expanded(
+    child: TextButton(
+        style: ButtonStyle(
+            backgroundColor:
+                MaterialStateProperty.all(getColorUsingIndex(index)),
+            shape: MaterialStateProperty.all(const RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(Radius.circular(2)),
+            ))),
+        onPressed: () {
+          final player = AudioPlayer();
+          player.play(AssetSource('note$index.wav'));
+        },
+        child: Text('')),
+  );
+}
+
+Color getColorUsingIndex(int index) {
+  Color color = Colors.red;
+  switch (index) {
+    case 1:
+      {
+        color = Colors.orange;
+      }
+      break;
+    case 2:
+      {
+        color = Colors.yellow;
+      }
+      break;
+    case 3:
+      {
+        color = Colors.green;
+      }
+      break;
+    case 4:
+      {
+        color = const Color.fromARGB(255, 157, 0, 255);
+      }
+      break;
+    case 5:
+      {
+        color = Colors.blue;
+      }
+      break;
+    case 6:
+      {
+        color = Colors.cyan;
+      }
+      break;
+    default:
+      color = Colors.red;
+      break;
+  }
+
+  return color;
 }
